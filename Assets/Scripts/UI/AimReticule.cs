a7ba90d15af6203e0bd0c9de5bb1d109733bbd30
Jack Evans <jackevans24@gmail.com>
2024-05-14 15:11:14 +0100

    feat: add aim reticule

---
+++ Assets/Scripts/UI/AimReticule.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace CliffJump.UI
+{
+    public class AimReticule : MonoBehaviour
+    {
+        [Header("Movement")]
+        [SerializeField] private float moveSpeed;
+        [SerializeField] private float smoothing;
+        
+        [Header("Input")]
+        [SerializeField] private InputActionReference movementReference;
+
+        private InputAction movementAction;
+        private Vector2 input, movement, currentVelocity;
+        
+        private void Awake()
+        {
+            movementAction = movementReference.ToInputAction();
+        }
+
+        private void OnEnable()
+        {
+            movementAction.Enable();
+        }
+
+        private void OnDisable()
+        {
+            movementAction.Disable();
+        }
+
+        private void Update()
+        {
+            input = movementAction.ReadValue<Vector2>();
+            movement = Vector2.SmoothDamp(movement, input * moveSpeed, ref currentVelocity, smoothing);
+        }
+
+        private void FixedUpdate()
+        {
+            transform.Translate(movement);
+        }
+    }
+}
