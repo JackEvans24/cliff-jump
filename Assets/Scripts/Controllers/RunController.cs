230b375a3ba342ca007516b40cbe94ff7e35b0f3
Jack Evans <jackevans24@gmail.com>
2024-05-14 11:41:41 +0100

    feat(run): add run controller and mash listener

---
+++ Assets/Scripts/Controllers/RunController.cs
@@ -0,0 +1,47 @@
+﻿using CliffJump.Input;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace CliffJump.Controllers
+{
+    public class RunController : MonoBehaviour
+    {
+        [SerializeField] private float runAcceleration = 2f, runDeceleration = 0.05f;
+        [SerializeField] private InputActionReference[] actionReferences;
+
+        private ButtonMashListener mashListener = new();
+
+        [SerializeField] private float currentRunSpeed;
+
+        private void Awake()
+        {
+            foreach (var actionReference in actionReferences)
+            {
+                var action = actionReference.ToInputAction();
+                mashListener.AddAction(action);
+            }
+
+            mashListener.ButtonMashed += OnMash;
+        }
+
+        private void OnEnable()
+        {
+            mashListener.Listen();
+        }
+
+        private void OnDisable()
+        {
+            mashListener.Unlisten();
+        }
+
+        private void FixedUpdate()
+        {
+            currentRunSpeed = Mathf.Max(0f, currentRunSpeed - runDeceleration);
+        }
+
+        private void OnMash()
+        {
+            currentRunSpeed += runAcceleration / Mathf.Max(currentRunSpeed, 1f);
+        }
+    }
+}
