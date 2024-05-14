9eb1fda8fdcbb68218493e28a6b71b4167b178ed
Jack Evans <jackevans24@gmail.com>
2024-05-14 16:17:00 +0100

    refactor: rename AimObstacle, pass reticule externally

---
+++ Assets/Scripts/UI/AimObstacle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CliffJump.UI
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class AimObstacle : MonoBehaviour
+    {
+        public bool ReticuleWithinBounds { get; private set; }
+        
+        [SerializeField] private ContactFilter2D contactFilter;
+
+        private Collider2D coll;
+        private AimReticule reticule;
+
+        private readonly List<Collider2D> overlaps = new();
+
+        public void SetReticule(AimReticule aimReticule) => reticule = aimReticule;
+
+        private void Awake()
+        {
+            coll = GetComponent<Collider2D>();
+        }
+
+        private void OnDisable()
+        {
+            ReticuleWithinBounds = false;
+        }
+
+        private void Update()
+        {
+            if (reticule == null)
+                return;
+
+            var overlapCount = coll.OverlapCollider(contactFilter, overlaps);
+            ReticuleWithinBounds = overlapCount > 0 && overlaps.Contains(reticule.Collider);
+        }
+    }
+}
