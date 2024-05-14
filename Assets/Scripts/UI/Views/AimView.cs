7dec848d61d6e3f9cf1350d2d95590c6562d09d4
Jack Evans <jackevans24@gmail.com>
2024-05-14 16:39:04 +0100

    feat: add cliff obstacle to aim view

--- Assets/Scripts/UI/Views/AimView.cs
+++ Assets/Scripts/UI/Views/AimView.cs
@@ -9,10 +9,11 @@ namespace CliffJump.UI.Views
     {
         [Header("References")]
         [SerializeField] private AimReticule reticule;
+
+        [SerializeField] private AimObstacle cliff;
         [SerializeField] private AimObstacle[] obstacles;
 
-        [Header("Field")]
-        [SerializeField] private int obstacleCount;
+        [Header("Field")] [SerializeField] private int obstacleCount;
 
         private List<int> allObstacleIndices;
         private readonly List<AimObstacle> obstaclesToCheck = new();
@@ -30,18 +31,21 @@ namespace CliffJump.UI.Views
             var obstacleIndices = new List<int>(allObstacleIndices);
             while (obstacleIndices.Count > obstacleCount)
                 obstacleIndices.RemoveAt(Random.Range(0, obstacleIndices.Count));
-            
+
             obstaclesToCheck.Clear();
 
             for (var i = 0; i < obstacles.Length; i++)
             {
                 var obstacle = obstacles[i];
                 obstacle.gameObject.SetActive(obstacleIndices.Contains(i));
-                
+
                 obstaclesToCheck.Add(obstacle);
             }
         }
 
-        public bool ReticuleOverlapsObstacle() => obstaclesToCheck.Any(ob => ob.ReticuleWithinBounds);
+        public bool ReticuleOverlapsObstacle()
+        {
+            return cliff.ReticuleWithinBounds || obstaclesToCheck.Any(ob => ob.ReticuleWithinBounds);
+        }
     }
 }
