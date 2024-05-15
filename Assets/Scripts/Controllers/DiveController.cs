c0faba8984c28764e140ae4818f0044136bc84d9
Jack Evans <jackevans24@gmail.com>
2024-05-15 16:39:12 +0100

    feat: allow input to change dive angle

---
+++ Assets/Scripts/Controllers/DiveController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace CliffJump.Controllers
+{
+    public class DiveController : MonoBehaviour
+    {
+        [Header("Input")]
+        [SerializeField] private InputActionReference tilt;
+
+        [Header("Tilt values")]
+        [SerializeField] private float tiltAmount;
+
+        private InputAction tiltAction;
+        
+        private float currentInput;
+        private float currentTiltAmount;
+
+        private void Awake()
+        {
+            tiltAction = tilt.ToInputAction();
+        }
+
+        private void OnEnable()
+        {
+            tiltAction.Enable();
+        }
+
+        private void OnDisable()
+        {
+            tiltAction.Disable();
+        }
+
+        private void Update()
+        {
+            currentInput = tiltAction.ReadValue<float>();
+        }
+
+        private void FixedUpdate()
+        {
+            currentTiltAmount += tiltAmount * currentInput;
+            Debug.Log(currentTiltAmount);
+        }
+    }
+}
