a5cb1290f390f6c0e457d31467c7040e1abe03fd
Jack Evans <jackevans24@gmail.com>
2024-05-15 13:15:06 +0100

    feat: convert jump controller to use data, add jump view to disable QTE text

--- Assets/Scripts/Controllers/JumpController.cs
+++ Assets/Scripts/Controllers/JumpController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using CliffJump.Data;
 using CliffJump.Input;
+using CliffJump.UI.Views;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Random = UnityEngine.Random;
@@ -9,35 +11,58 @@ namespace CliffJump.Controllers
 {
     public class JumpController : MonoBehaviour
     {
+        [Header("References")]
+        [SerializeField] private JumpView view;
+        
         [Header("Input")]
-        [SerializeField] private InputActionReference[] actionReferences;
         [SerializeField] private int qteLength = 3;
+        [SerializeField] private QTEAction[] qteActions;
 
-        private InputAction[] actions;
         private readonly QTEListener qteListener = new();
-
-        private void Start()
-        {
-            actions = actionReferences
-                .Select(actionRef => actionRef.ToInputAction())
-                .ToArray();
-
-            var qte = GenerateQteQueue();
-            qteListener.Listen(actions, qte);
-        }
 
         private void OnEnable()
         {
             qteListener.Progress += OnQteProgress;
             qteListener.Succeeded += OnQteSucceeded;
             qteListener.Failed += OnQteFailed;
+            
+            StartQTE();
         }
 
         private void OnDisable()
         {
+            qteListener.Unlisten();
+
             qteListener.Progress -= OnQteProgress;
             qteListener.Succeeded -= OnQteSucceeded;
             qteListener.Failed -= OnQteFailed;
+        }
+
+        private void StartQTE()
+        {
+            var actions = qteActions
+                .Select(action => action.ActionReference.ToInputAction())
+                .ToArray();
+            
+            view.ClearView();
+
+            var inputActions = GenerateQteQueue();
+            qteListener.Listen(actions, inputActions);
+        }
+
+        private Queue<InputAction> GenerateQteQueue()
+        {
+            var inputActions = new Queue<InputAction>();
+
+            for (var i = 0; i < qteLength; i++)
+            {
+                var action = qteActions[Random.Range(0, qteActions.Length)];
+                inputActions.Enqueue(action.ActionReference.ToInputAction());
+                
+                view.AddQTELabel(action);
+            }
+
+            return inputActions;
         }
 
         private void OnQteProgress()
@@ -48,27 +73,13 @@ namespace CliffJump.Controllers
         private void OnQteSucceeded()
         {
             Debug.Log("SUCCESS");
-            
             qteListener.Unlisten();
         }
 
         private void OnQteFailed()
         {
             Debug.Log("FAILURE");
-        }
-
-        private Queue<InputAction> GenerateQteQueue()
-        {
-            var qte = new Queue<InputAction>();
-            for (var i = 0; i < qteLength; i++)
-            {
-                var action = actions[Random.Range(0, actions.Length)];
-                qte.Enqueue(action);
-                
-                Debug.Log($"Adding QTE action: {action.name}");
-            }
-
-            return qte;
+            qteListener.Unlisten();
         }
     }
 }
---
+++ Assets/Scripts/UI/Views/JumpView.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using CliffJump.Data;
+using TMPro;
+using UnityEngine;
+
+namespace CliffJump.UI.Views
+{
+    public class JumpView : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Canvas canvas;
+        [SerializeField] private TMP_Text textPrefab;
+
+        [Header("Spacing")]
+        [SerializeField] private Vector2 origin;
+        [SerializeField] private Vector2 offset;
+        
+        private readonly List<TMP_Text> currentPrefabs = new();
+
+        public void ClearView()
+        {
+            foreach (var tmp in currentPrefabs)
+                Destroy(tmp.gameObject);
+
+            currentPrefabs.Clear();
+        }
+
+        public void AddQTELabel(QTEAction action)
+        {
+            var tmp = Instantiate(textPrefab, canvas.transform);
+            tmp.text = action.Label;
+
+            tmp.transform.localPosition = origin + (offset * currentPrefabs.Count);
+            
+            currentPrefabs.Add(tmp);
+        }
+    }
+}
