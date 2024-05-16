582097c139c7b590f947634715d9d380a73ece26
Jack Evans <jackevans24@gmail.com>
2024-05-16 12:01:33 +0100

    refactor: remove angle text label

--- Assets/Scripts/UI/Views/DiveView.cs
+++ Assets/Scripts/UI/Views/DiveView.cs
@@ -1,13 +1,10 @@
-﻿using System.Text;
-using TMPro;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace CliffJump.UI.Views
 {
     public class DiveView : MonoBehaviour
     {
         [Header("References")]
-        [SerializeField] private TMP_Text label;
         [SerializeField] private Transform indicator;
         [SerializeField] private Transform maxIndicator;
         [SerializeField] private Transform minIndicator;
@@ -23,31 +20,7 @@ namespace CliffJump.UI.Views
 
         public void SetUI(float diveAngle)
         {
-            SetLabel(diveAngle);
             SetIndicatorRotation(diveAngle);
-        }
-
-        private void SetLabel(float diveAngle)
-        {
-            var sb = new StringBuilder();
-            
-            if (diveAngle < -20f)
-                sb.Append(">");
-            if (diveAngle < -10f)
-                sb.Append(">");
-            if (diveAngle < -0f)
-                sb.Append("> ");
-
-            sb.Append(diveAngle.ToString("0.0"));
-            
-            if (diveAngle > 0f)
-                sb.Append(" <");
-            if (diveAngle > 10f)
-                sb.Append("<");
-            if (diveAngle > 20f)
-                sb.Append("<");
-
-            label.text = sb.ToString();
         }
 
         private void SetIndicatorRotation(float diveAngle)
