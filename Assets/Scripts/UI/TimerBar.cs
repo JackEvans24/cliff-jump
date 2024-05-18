be1d3ff5414990019d00713d861a74e2c3c32b40
Jack Evans <jackevans24@gmail.com>
2024-05-18 09:55:12 +0100

    feat: fade TimerBar.cs in/out

--- Assets/Scripts/UI/TimerBar.cs
+++ Assets/Scripts/UI/TimerBar.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 
 namespace CliffJump.UI
 {
-    public class TimerBar: MonoBehaviour
+    public class TimerBar : MonoBehaviour
     {
+        [Header("References")]
+        [SerializeField] private Transform parent;
         [SerializeField] private Transform bar;
-        [SerializeField] private Transform background;
+        [SerializeField] private SpriteRenderer[] sprites;
 
+        [Header("Fade")]
+        [SerializeField][Min(0.01f)] private float fadeTime = 0.1f;
+
+        [Header("Shake")]
         [SerializeField][Range(0f, 1f)] private float shakeThreshold = 0.5f;
         [SerializeField] private float shakeStrength = 1f;
         [SerializeField] private float shakeTime = 0.5f;
@@ -24,7 +30,9 @@ namespace CliffJump.UI
             shaking = false;
             shakeRotation = new Vector3(0f, 0f, shakeStrength);
 
-            background.DOKill();
+            parent.DOKill();
+
+            FadeSprites(true);
             
             UpdateTimer(timerLength);
         }
@@ -39,10 +47,19 @@ namespace CliffJump.UI
                 shaking = true;
 
                 DOTween.Sequence()
-                    .Append(background.DORotate(shakeRotation, shakeTime))
-                    .Append(background.DORotate(-shakeRotation, shakeTime * 2))
-                    .Append(background.DORotate(Vector3.zero, shakeTime))
+                    .Append(parent.DORotate(shakeRotation, shakeTime))
+                    .Append(parent.DORotate(-shakeRotation, shakeTime * 2))
+                    .Append(parent.DORotate(Vector3.zero, shakeTime))
                     .SetLoops(Mathf.RoundToInt(timeRemaining / (shakeTime * 4)), LoopType.Restart);
+            }
+        }
+
+        public void FadeSprites(bool enable)
+        {
+            foreach (var spriteRenderer in sprites)
+            {
+                spriteRenderer.DOKill();
+                spriteRenderer.DOFade(enable ? 1f : 0f, fadeTime);
             }
         }
     }
