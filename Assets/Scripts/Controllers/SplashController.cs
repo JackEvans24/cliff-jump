cce1ede538ba5176e8f58ae2f2d37fa3b5d4e0f6
Jack Evans <jackevans24@gmail.com>
2024-05-19 18:28:08 +0100

    chore: add end game triggers to splash controller

--- Assets/Scripts/Controllers/SplashController.cs
+++ Assets/Scripts/Controllers/SplashController.cs
@@ -1,5 +1,6 @@
 using CliffJump.UI;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace CliffJump.Controllers
 {
@@ -10,11 +11,18 @@ namespace CliffJump.Controllers
         [SerializeField] private GameObject rockObj;
         [SerializeField] private GameObject boatObj;
 
+        public UnityEvent triggerWinScreen;
+        public UnityEvent triggerLoseScreen;
+        
         public void SetAnimationObjects(ObstacleType obstacleType)
         {
             successObj.SetActive(obstacleType == ObstacleType.None);
             rockObj.SetActive(obstacleType == ObstacleType.Rock || obstacleType == ObstacleType.Cliff);
             boatObj.SetActive(obstacleType == ObstacleType.Boat);
         }
+
+        public void TriggerWin() => triggerWinScreen?.Invoke();
+
+        public void TriggerLose() => triggerLoseScreen?.Invoke();
     }
 }
