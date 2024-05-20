6a1c4a2ddcc0128898418eecb578a2b92c72f3d1
Jack Evans <jackevans24@gmail.com>
2024-05-20 14:22:20 +0100

    feat: add splash and bonk sounds to result animations

--- Assets/Scripts/Camera/FeedbackTrigger.cs
+++ Assets/Scripts/Camera/FeedbackTrigger.cs
@@ -6,7 +6,13 @@ namespace CliffJump.Camera
     public class FeedbackTrigger : MonoBehaviour
     {
         public UnityEvent triggerFeedback;
+        public UnityEvent triggerSound;
+        public UnityEvent triggerAltSound;
 
         public void TriggerFeedback() => triggerFeedback?.Invoke();
+
+        public void TriggerSound() => triggerSound?.Invoke();
+
+        public void TriggerAltSound() => triggerAltSound?.Invoke();
     }
 }
--- Assets/Scripts/Controllers/SplashController.cs
+++ Assets/Scripts/Controllers/SplashController.cs
@@ -12,6 +12,11 @@ namespace CliffJump.Controllers
         [SerializeField] private GameObject boatObj;
         [SerializeField] private GameObject tiltObj;
         [SerializeField] private GameObject cliffObj;
+        [SerializeField] private AudioSource audioSource;
+
+        [Header("Sounds")]
+        [SerializeField] private AudioClip bonk;
+        [SerializeField] private AudioClip splash;
 
         public UnityEvent triggerWinScreen;
         public UnityEvent triggerLoseScreen;
@@ -32,5 +37,9 @@ namespace CliffJump.Controllers
         public void TriggerLose() => triggerLoseScreen?.Invoke();
 
         public void TriggerFeedback() => triggerFeedback?.Invoke();
+
+        public void PlayBonk() => audioSource.PlayOneShot(bonk);
+
+        public void PlaySplash() => audioSource.PlayOneShot(splash);
     }
 }
