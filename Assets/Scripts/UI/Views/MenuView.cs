96fa0cff8855764502a231d3ed4a1b264a507d84
Jack Evans <jackevans24@gmail.com>
2024-05-20 12:26:30 +0100

    feat: fade ui on main menu transition

--- Assets/Scripts/UI/Views/MenuView.cs
+++ Assets/Scripts/UI/Views/MenuView.cs
@@ -1,5 +1,5 @@
-using System;
 using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,18 +7,36 @@ namespace CliffJump.UI.Views
 {
     public class MenuView : MonoBehaviour
     {
+        [Header("References")]
+        [SerializeField] private CanvasGroup canvas;
+
+        [Header("Animation")]
+        [SerializeField] private float uiFadeTime = 0.1f;
+
+        [Header("Callbacks")]
         public UnityEvent onStartGameRequested;
+
+        private void OnEnable()
+        {
+            canvas.DOKill();
+            
+            canvas.alpha = 1f;
+            canvas.interactable = true;
+        }
 
         public void StartGame()
         {
             // TODO: Disable UI
+            canvas.interactable = false;
 
             StartCoroutine(DoStartGame());
         }
 
         private IEnumerator DoStartGame()
         {
-            yield return null;
+            yield return canvas
+                .DOFade(0f, uiFadeTime)
+                .WaitForCompletion();
             
             onStartGameRequested?.Invoke();
         }
